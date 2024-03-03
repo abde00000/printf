ab173563bb295d67322fca7ecc0ee96c161c56d7
abdessamad <abdessamadelmahboub1999@gmail.com>
2024-03-04 00:03:05 +0100

    update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -26,29 +26,29 @@ unsigned int _strlen(char *str)
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *tmp;
-
-	if (str == NULL)
-		return (NULL);
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-	new->str = strdup(str);
-	if (new->str == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	new->len = _strlen(new->str);
-	new->next = NULL;
-	if (*head == NULL)
-	{
-		*head = new;
-		return (new);
-	}
-	tmp = *head;
-	while (tmp->next)
-		tmp = tmp->next;
-	tmp->next = new;
-	return (new);
+    list_t *new;
+    
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+    {
+        return NULL;
+    }
+    new->len = strlen(str);
+    new->str = strdup(str);
+    if (*head == NULL)
+    {
+        *head = new;
+        
+    }
+    else
+    {
+        list_t *current = *head;
+        while (current->next != NULL)
+        {
+            current= current->next;
+        }
+        current->next = new;
+        new->next = NULL;
+    }
+    return new;
 }
